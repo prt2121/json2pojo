1d071c76d1fe5e678c75cf124193626a2c639017
Prat <banqfirn@gmail.com>
2015-11-08 22:53:01 -0500

    Fix the flow

--- src/main/kotlin/com/prt2121/pojo/MainController.kt
+++ src/main/kotlin/com/prt2121/pojo/MainController.kt
@@ -2,12 +2,14 @@ package com.prt2121.pojo
 
 import com.prt2121.pojo.rx.JavaFxObservable
 import com.sun.codemodel.JCodeModel
-import javafx.event.ActionEvent
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Button
+import javafx.scene.control.ProgressIndicator
 import javafx.scene.control.TextArea
 import javafx.scene.control.TextField
+import javafx.scene.input.MouseEvent
 import org.jsonschema2pojo.*
 import org.jsonschema2pojo.rules.RuleFactory
 import java.io.*
@@ -25,6 +27,8 @@ public class MainController : Initializable {
   @FXML var packageNameField: TextField? = null
   @FXML var generateButton: Button? = null
   @FXML var classNameField: TextField? = null
+  @FXML var progressIndicator: ProgressIndicator? = null
+
 
   val codeModel = JCodeModel()
   val mapper = SchemaMapper(RuleFactory(object : DefaultGenerationConfig() {
@@ -34,7 +38,6 @@ public class MainController : Initializable {
   }, GsonAnnotator(), SchemaStore()), SchemaGenerator())
 
   override fun initialize(url: URL?, bundle: ResourceBundle?) {
-
     var console = Console(consoleTextArea!!)
     var ps = PrintStream(console, true)
     System.setOut(ps)
@@ -42,38 +45,64 @@ public class MainController : Initializable {
 
     val jsonObservable = JavaFxObservable
         .fromObservableValue(jsonTextArea!!.textProperty())
-        .doOnNext { generateButton?.isDisable == it.isEmpty() }
-        .filter { it.isNotBlank() }
+        .throttleLast(1, TimeUnit.SECONDS)
 
     val classNameObservable = JavaFxObservable
         .fromObservableValue(classNameField!!.textProperty())
-        .doOnNext { generateButton?.isDisable == it.isEmpty() }
-        .filter { it.isNotBlank() }
+        .throttleLast(1, TimeUnit.SECONDS)
 
     val packageNameObservable = JavaFxObservable
         .fromObservableValue(packageNameField!!.textProperty())
+        .throttleLast(1, TimeUnit.SECONDS)
 
     val clicks = JavaFxObservable
-        .fromNodeEvents(generateButton!!, ActionEvent.ACTION)
-        .throttleLast(3, TimeUnit.SECONDS)
+        .fromNodeEvents(generateButton!!, MouseEvent.MOUSE_CLICKED)
+        .throttleLast(1, TimeUnit.SECONDS)
 
-    rx.Observable.combineLatest(jsonObservable, packageNameObservable, classNameObservable, clicks) {
-      json, packageName, className, clicks ->
+    rx.Observable.combineLatest(jsonObservable, packageNameObservable, classNameObservable) {
+      json, packageName, className ->
+      Input(json, packageName, className)
+    }.doOnNext {
+      Platform.runLater {
+        generateButton?.setDisable(it.json.isBlank() || it.className.isBlank())
+      }
+    }.subscribe()
+
+    clicks.map {
+      val packageName = packageNameField?.textProperty()?.value ?: ""
+      Input(jsonTextArea!!.textProperty().value, packageName, classNameField!!.textProperty().value)
+    }.doOnNext {
+      Platform.runLater {
+        progressIndicator!!.isVisible = true
+      }
+    }.map {
+      input ->
       val dir = File("json2pojoOutput")
-      removeOldOutput(dir)
+      dir.deleteRecursively()
       dir.mkdir()
-      val inputFile = createInputFile(json)
-      mapper.generate(codeModel, className, packageName, inputFile.toURI().toURL())
+      val inputFile = createInputFile(input.json)
+      mapper.generate(codeModel, input.className, input.packageName, inputFile.toURI().toURL())
       codeModel.build(dir)
       inputFile.delete()
       dir
     }.subscribe({
       file ->
       println(file.absolutePath)
+      Platform.runLater {
+        progressIndicator!!.isVisible = false
+      }
     }, {
       t ->
-      print(t.message)
+      println(t.message)
+      Platform.runLater {
+        progressIndicator!!.isVisible = false
+      }
+    }, {
+      Platform.runLater {
+        progressIndicator!!.isVisible = false
+      }
     })
+    
   }
 
   // https://github.com/joelittlejohn/jsonschema2pojo/issues/255
@@ -86,26 +115,32 @@ public class MainController : Initializable {
     return inputFile
   }
 
-  private fun removeOldOutput(targetDirectory: File) {
-    fun delete(f: File) {
-      if (f.isDirectory) {
-        for (child in f.listFiles()!!) {
-          delete(child)
-        }
-      }
-      f.delete()
-    }
-    if (targetDirectory.exists()) {
-      for (f in targetDirectory.listFiles()!!) {
-        delete(f)
-      }
-    }
-  }
-
   class Console(private val output: TextArea) : OutputStream() {
     @Throws(IOException::class)
     override fun write(i: Int) {
       output.appendText(i.toChar().toString())
+    }
+  }
+
+  class Input(val json: String, val packageName: String, val className: String) {
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (other?.javaClass != javaClass) return false
+
+      other as Input
+
+      if (json != other.json) return false
+      if (packageName != other.packageName) return false
+      if (className != other.className) return false
+
+      return true
+    }
+
+    override fun hashCode(): Int {
+      var result = json.hashCode()
+      result += 31 * result + packageName.hashCode()
+      result += 31 * result + className.hashCode()
+      return result
     }
   }
 
